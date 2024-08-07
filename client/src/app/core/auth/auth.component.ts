43afb95b003e3c9b72a650a108107fe22782c52d
Lakshan Ruwinda <112312002+shan-ganegoda@users.noreply.github.com>
2024-08-07 20:48:53 +0530

    update: Login window Update

--- client/src/app/core/auth/auth.component.ts
+++ client/src/app/core/auth/auth.component.ts
@@ -3,7 +3,7 @@ import {MatGridListModule} from "@angular/material/grid-list";
 import {MatCardModule} from "@angular/material/card";
 import {MatInputModule} from "@angular/material/input";
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {ActivatedRoute, Router} from "@angular/router";
+import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 import {AuthService} from "../service/auth/auth.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {NotificationComponent} from "../../shared/dialog/notification/notification.component";
@@ -22,7 +22,8 @@ import {ToastService} from "../util/toast/toast.service";
     MatGridListModule,
     MatCardModule,
     MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
